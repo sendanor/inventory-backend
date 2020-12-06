601fc8b0e6c3ba48c0bfa622eb6b74f1c6eae5af
Jaakko Heusala <jheusala@iki.fi>
2020-12-06 09:28:32 +0200

    server.ts: Added error handling and informative log message about started server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import { createRepository } from "./PgHostRepository"
 import HostController from "./HostController"
-import { ProcessUtils } from './ProcessUtils'
+import { ProcessUtils } from './services/ProcessUtils'
 import HTTP = require('http')
+import {IB_LISTEN_HOSTNAME, IB_LISTEN_PORT} from "./constants/env";
+import LogService from "./services/LogService";
 
-ProcessUtils.initEnvFromDefaultFiles()
-const controller = new HostController(createRepository())
-const server = HTTP.createServer(controller.requestListener.bind(controller))
-server.listen(3000, 'localhost')
+const LOG = LogService.createLogger('server');
+
+try {
+
+    ProcessUtils.initEnvFromDefaultFiles();
+    const controller = new HostController(createRepository());
+    const server = HTTP.createServer(controller.requestListener.bind(controller));
+    server.listen(IB_LISTEN_PORT, IB_LISTEN_HOSTNAME);
+
+    LOG.info(`Listening at http://${IB_LISTEN_HOSTNAME}:${IB_LISTEN_PORT}`);
+
+} catch(err) {
+    LOG.error('ERROR: ', err);
+}
