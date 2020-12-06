d6dd00f0b6fcad89705a048b2197a2acf2fa911e
Jaakko Heusala <jheusala@iki.fi>
2020-12-06 09:24:56 +0200

    HostRepository: Fixed interface definition to match the implementation

--- src/HostRepository.ts
+++ src/HostRepository.ts
@@ -2,7 +2,7 @@ import Host, { HostPage, HostSaveResult } from './Host';
 
 export interface HostRepository {
     initialize(): void
-    get(id: string): Promise<Host>
+    get(id: string): Promise<Host|undefined>
     getPage(page: number, size: number): Promise<HostPage>
     create(host: Host): Promise<HostSaveResult>
     update(id: string, host: Host): Promise<HostSaveResult>
